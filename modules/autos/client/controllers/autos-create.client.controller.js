82477e31f06102b1cc6f74dffb083ec2451fd7d8
quyhpES <quyhp@eplatform.vn>
2017-10-05 17:31:10 +0700

    update auto create

--- modules/autos/client/controllers/autos-create.client.controller.js
+++ modules/autos/client/controllers/autos-create.client.controller.js
@@ -33,8 +33,8 @@
 
     vm.setting = {
       start: 0,
-      time: 1000,
-      spacing: 1000
+      time: 200,
+      spacing: 300
     };
 
     vm.autos = [{
@@ -43,12 +43,8 @@
       start: vm.setting.start,
       end: vm.setting.start + vm.setting.time,
       time: vm.setting.time,
-      isMove: false,
-      move: {
-        X: 0,
-        Y: 0,
-        before: 100
-      },
+      before: 0,
+      moves: [],
       touchs: []
     }];
 
@@ -58,26 +54,21 @@
       auto.start = last.end + vm.setting.spacing;
       auto.time = vm.setting.time;
       auto.end = auto.start + auto.time;
-      auto.isMove = false;
-      auto.move = {
-        X: 0,
-        Y: 0,
-        time: 400
-      };
+      auto.before = vm.setting.spacing;
+      auto.moves = [];
       auto.touchs = [];
 
       vm.autos.push(auto);
     };
 
     $scope.reloadAutos = function () {
-      console.log('xxxxxx');
       vm.autos.forEach(function (auto, index) {
         if (index === 0) {
-          auto.start = vm.setting.start;
-          auto.end = vm.setting.start + auto.time;
+          auto.start = auto.before;
+          auto.end = auto.start + auto.time;
         } else {
           var last = vm.autos[index - 1];
-          auto.start = last.end + vm.setting.spacing;
+          auto.start = last.end + auto.before;
           auto.end = auto.start + auto.time;
         }
       });
@@ -89,13 +80,13 @@
         var startLine = '' + START.A + auto.X + MID + auto.Y + START.B + auto.start;
         vm.render += startLine + '\n';
 
-        if (auto.isMove) {
-          var moveLine = '' + MOVE.A + auto.move.X + MID + auto.move.Y + MOVE.B + (auto.start + auto.move.before);
+        auto.moves.forEach(function (move) {
+          var moveLine = '' + MOVE.A + move.X + MID + move.Y + MOVE.B + (auto.start + move.before);
           vm.render += moveLine + '\n';
-        }
+        });
 
         auto.touchs.forEach(function (touch) {
-          var V = auto.isMove ? { X: auto.X, Y: auto.Y } : { X: auto.move.X, Y: auto.move.Y }
+          var V = auto.moves.length === 0 ? { X: auto.X, Y: auto.Y } : { X: auto.moves[auto.moves.length - 1].X, Y: auto.moves[auto.moves.length - 1].Y }
           var lineA = '' + TOUCH.A + V.X + MID + V.Y + MID + touch.X + MID + touch.Y + TOUCH.B + (auto.start + touch.before);
           vm.render += lineA + '\n';
           var lineB = '' + TOUCH.C + V.X + MID + V.Y + TOUCH.D + (auto.start + touch.before + touch.time);
@@ -110,8 +101,39 @@
 
       var blob = new Blob([vm.render], { type: 'text/plain' });
       var url = ($window.URL || $window.webkitURL).createObjectURL(blob);
-      window.open(url);
-      vm.url = url;
+      $window.open(url);
+    };
+
+    $scope.import = function (selectedFiles) {
+      if (selectedFiles) {
+
+        var reader = new FileReader();
+        reader.readAsText(selectedFiles);
+
+        reader.onload = function () {
+          var result = reader.result.trim().replace(/ /g, '').replace(/\n\n/g, '\n');
+          result = result.split('\n');
+
+          var listStrings = [];
+          result.forEach(function (value) {
+            if (value && value !== '') {
+              listStrings.push(value);
+            }
+          });
+
+          arrayStringToArrayAuto(listStrings);
+        };
+
+      }
+    };
+
+    $scope.addMoves = function (auto) {
+      var move = {
+        X: 0,
+        Y: 0,
+        before: 200
+      };
+      auto.moves.push(move);
     };
 
     $scope.addTouchs = function (auto) {
@@ -123,5 +145,84 @@
       };
       auto.touchs.push(touch);
     };
+
+    function arrayStringToArrayAuto(listStrings) {
+      vm.autos = [];
+      var auto = {};
+      var touch = {};
+
+      for (let i in listStrings) {
+        var line = listStrings[i];
+
+        if (line.indexOf('MULTI:1:0:') !== -1) {
+          var position = getPositionStartFromLine(line);
+          auto = { X: position.X, Y: position.Y, start: position.time, moves: [], touchs: [] };
+          auto.before = vm.autos.length !== 0 ? auto.start - vm.autos[vm.autos.length - 1].end : auto.start
+
+        } else if (line.indexOf('MSBRL') !== -1) {
+          auto.end = getEndFromLine(line);
+          auto.time = auto.end - auto.start;
+          vm.autos.push(auto);
+
+        } else if (line.indexOf('MULTI:1:2:') !== -1) {
+          var position = getPositionStartFromLine(line);
+          auto.moves.push({
+            X: position.X,
+            Y: position.Y,
+            before: position.time - auto.start
+          });
+
+        } else if (line.indexOf('MULTI:2:5:') !== -1) {
+          var position = getPositionStartFromLine(line);
+          touch = {
+            X: position.X,
+            Y: position.Y,
+            before: position.time - auto.start,
+            time: 200
+          };
+          auto.touchs.push(touch);
+
+        } else if (line.indexOf('MULTI:1:6:') !== -1) {
+          var end = getEndFromLine(line);
+          touch.time = end - auto.start - touch.before;
+          //  auto.touchs.push(touch);
+        }
+
+      }
+
+      $scope.$apply();
+    }
+
+    function getPositionStartFromLine(line) {
+      var key = 'ScRiPtSePaRaToR';
+      var index = line.lastIndexOf(key);
+
+      var X, Y, time;
+      var p = -1;
+
+      for (let i = index - 1; i > 0; i--) {
+        if (isNaN(line[i]) === true) {
+          if (p === -1) {
+            Y = parseInt(line.slice(i + 1, index), 10);
+            p = i;
+          } else {
+            X = parseInt(line.slice(i + 1, p), 10);
+            break;
+          }
+        }
+      }
+
+      time = parseInt(line.slice(index + key.length, line.length), 10);
+
+      return { X: X, Y: Y, time: time };
+    }
+
+    function getEndFromLine(line) {
+      var key = 'ScRiPtSePaRaToR';
+      var index = line.lastIndexOf(key);
+
+      return parseInt(line.slice(index + key.length, line.length), 10);
+    }
+
   }
 }());
